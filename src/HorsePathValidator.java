3433730d91b558b5a3be888c69b1cfd3f42b1903
StefanTodorov988 <stodorov988@gmail.com>
2018-02-07 17:31:34 +0200

    Initial commit

---
+++ src/HorsePathValidator.java
@@ -0,0 +1,91 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HorsePathValidator {//Not completed
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        Integer n = Integer.parseInt(in.readLine());
+        System.out.println(countOfPaths(n));
+    }
+    public static int countOfPaths(int n){
+        List<Integer> list = new ArrayList<Integer>();
+        int[][] matrix = new int[n][n];
+        list.add(0);
+        tracePath(n,matrix,0, list,0,0);
+        return list.get(0);
+    }
+    public static void tracePath(int n,int[][] matrix, int counter ,List<Integer> list , int currentI , int currentJ ){//wrapping only 0 index
+
+        if (isLegit(matrix, currentI - 2, currentJ - 1) && matrix[currentI - 2][currentJ - 1] == 0) {
+            counter++;
+            matrix[currentI][currentJ] = counter;
+            currentI = currentI-2;
+            currentJ = currentJ-1;
+            tracePath(n , matrix ,counter, list,currentI,currentJ);
+        } else if (isLegit(matrix, currentI - 2, currentJ + 1) && matrix[currentI - 2][currentJ + 1] == 0) {
+            counter++;
+            matrix[currentI][currentJ] = counter;
+            currentI = currentI-2;
+            currentJ = currentJ+1;
+            tracePath(n , matrix ,counter, list,currentI,currentJ);
+        } else if (isLegit(matrix, currentI - 1, currentJ - 2) && matrix[currentI - 1][currentJ - 2] == 0) {
+            counter++;
+            matrix[currentI][currentJ] = counter;
+            currentI = currentI-1;
+            currentJ = currentJ-2;
+            tracePath(n , matrix ,counter, list,currentI,currentJ);
+        } else if (isLegit(matrix, currentI - 1, currentJ + 2) && matrix[currentI - 1][currentJ + 2] == 0) {
+            counter++;
+            matrix[currentI][currentJ] = counter;
+            currentI = currentI-1;
+            currentJ = currentJ+2;
+            tracePath(n , matrix ,counter, list,currentI,currentJ);
+        } else if (isLegit(matrix, currentI + 1, currentJ - 2) && matrix[currentI + 1][currentJ - 2] == 0) {
+            counter++;
+            matrix[currentI][currentJ] = counter;
+            currentI = currentI+1;
+            currentJ = currentJ-2;
+            tracePath(n , matrix ,counter, list,currentI,currentJ);
+        } else if (isLegit(matrix, currentI + 1, currentJ + 2) && matrix[currentI + 1][currentJ + 2] == 0) {
+            counter++;
+            matrix[currentI][currentJ] = counter;
+            currentI = currentI+1;
+            currentJ = currentJ+2;
+            tracePath(n , matrix ,counter, list,currentI,currentJ);
+        } else if (isLegit(matrix, currentI + 2, currentJ - 1) && matrix[currentI + 2][currentJ - 1] == 0) {
+            counter++;
+            matrix[currentI][currentJ] = counter;
+            currentI = currentI+2;
+            currentJ = currentJ-1;
+            tracePath(n , matrix ,counter, list,currentI,currentJ);
+        } else if (isLegit(matrix, currentI + 2, currentJ + 1) && matrix[currentI + 2][currentJ + 1] == 0) {
+            counter++;
+            matrix[currentI][currentJ] = counter;
+            currentI = currentI+2;
+            currentJ = currentJ+1;
+            tracePath(n , matrix ,counter, list,currentI,currentJ);
+        } else {
+            if (counter == (n*n)){
+                list.set(0,list.get(0) + 1);
+            }
+        }
+    }
+    public static boolean isLegit(int[][] matrix, int i, int j) {
+        if (i < 0) {
+            return false;
+        }
+        if (i >= matrix.length) {
+            return false;
+        }
+        if (j < 0) {
+            return false;
+        }
+        if (j >= matrix[i].length) {
+            return false;
+        }
+        return true;
+    }
+}
--- src/Test.java
+++ src/Test.java
@@ -1,35 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Test {
-    public static void main(String[] args) {
-        List<String> list = Arrays.asList(
-                "Stefko",
-                "Pefko"
-        );
-        System.out.println(list.get(0) + " " + list.get(1));
-        doSomething(list);
-        System.out.println(list.get(0) + " " + list.get(1));
-        /////////////////////////////////////////////////////////
-        Person pefko = new Person("Pefko");
-        doSomethingSecond(pefko);
-        System.out.println(pefko.name);
+    public static void main(String[] args) throws IOException {
+       BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        List<String> list = Arrays.asList(in.readLine().split(" "));
+
+        List<Person> arr = list.stream().map(x -> new Person(x)).filter(Person.distinctByKey(Person::getName)).collect(Collectors.toList());
+
+        arr.stream().map(x -> x.name = "Teko").collect(Collectors.toList());
+        arr.forEach(x-> x.name = "Teko");
+        arr.forEach(x -> System.out.println(x.name));
     }
 
-    public static void doSomething(List<String> asd){
-        asd.set(0,"Pefko");
-        asd.set(1,"Stefko");
+    public static void doSomething(List<String> asd) {
+        asd.set(0, "Pefko");
+        asd.set(1, "Stefko");
 
     }
-    public static void doSomethingSecond(Person person){
-        person= new Person("Stefko");
+
+    public static void doSomethingSecond(Person person) {
+        person = new Person("Stefko");
     }
 }
-class Person{
+
+class Person implements Comparator<Person> {
     public String name;
-    public Person(String name ){
+    private static int counter = 0;
+    private int selfCounter;
+
+    public Person(String name) {
         this.name = name;
+        counter++;
+        selfCounter = counter;
+    }
+
+    public int getCounter() {
+        return selfCounter;
+    }
+    public String getName() {
+        return name;
+    }
+    @Override
+    public int compare(Person o1, Person o2) {
+        if (o1.name == o2.name) {
+            return 0;
+        }
+        return 1;
+    }
+    public static <Person> Predicate<Person> distinctByKey(Function<? super Person, ?> keyExtractor) {
+        Set<Object> seen = ConcurrentHashMap.newKeySet();
+        return t -> seen.add(keyExtractor.apply(t));
     }
 }
