cce3309ed8a0cceff663762bccd003c6e96fb81e
StefanTodorov988 <todorov.stefan@yahoo.com>
2018-02-08 15:26:07 +0200

    Initial commit

---
+++ src/AppearanceCount.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AppearanceCount {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        Integer n = Integer.parseInt(in.readLine());
+        Map<String , Integer> map = new HashMap<>();
+        for (String str:in.readLine().split(" ")) {
+            if (map.containsKey(str)){
+                map.put(str,map.get(str) + 1);
+            }
+            else{
+                map.put(str,1);
+            }
+        }
+        String searchedNum = in.readLine();
+        System.out.println(map.containsKey(searchedNum)? map.get(searchedNum): 0);
+    }
+}
